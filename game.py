b577e9f808a6f4fe3d08eea000255a75650fc40b
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-08-07 19:41:36 +0200

    Corrected the QWERTY layout

--- game.py
+++ game.py
@@ -49,7 +49,7 @@ class Application():
                     ("W","X","C","V","B","N")]
         
         # QWERTY keyboard layout
-        qwerty = [("Q","W","R","T","Y","U","I","O","P"),
+        qwerty = [("Q","W","E","R","T","Y","U","I","O","P"),
                   ("A","S","D","F","G","H","J","K","L"),
                   ("Z","X","C","V","B","N","M")]
         # Game keyboard
